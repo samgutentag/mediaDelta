2db3e8704b9b09e783610c4f0139824d0c15cda3
Sam Gutentag <samgutentag@me.com>
2016-10-27 22:09:15 -0700

    heavy file cleanup and github repo cleanup

--- pyExifToolTest.py
+++ pyExifToolTest.py
@@ -23,21 +23,116 @@ def openMediaFile(parser, arg):
         return open(arg, 'rb')
 
 # print tags
-def prettyPrintTags(data):
-    print (json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+def prettyPrintTags(dataDictionary):
+    # print dataDictionary
+    for key, value in dataDictionary.iteritems():
+        print '%s\t:\t%s' % (key, value)
 
-def prettyPrintTags2(data):
-    counter = 0
-    for item in sorted(data[0].iteritems()):
 
-        tag = item[0]
-        info = item[-1]
 
-        print '%i:\t%s' % (counter, item)
-        print '%i:\t%s\t%s' % (counter, tag, info)
 
-        print
-        counter += 1
+def JSONToDict(data):
+    print 'creating exifDict from json data...'
+
+    if len(data) < 1:
+        print 'this file has no exif data'
+        return
+    else:
+        # dataDict = dict(sorted(data[0].iteritems()))
+        dataDict = dict(sorted(data[0].iteritems()))
+        print type(dataDict)
+        print dataDict
+        return dataDict
+
+
+#------------------------------------------------------------------------------
+#		main data points functions
+#------------------------------------------------------------------------------
+
+def getCameraModel(data):
+    print 'getting camera info...'
+
+    # get tags with the word 'date' in them
+
+
+
+def getMediaDateTimeStamp(data):
+    print 'getting earliest media time stamp...'
+
+    # collect all date relted keys and values
+    dateTimeTags = []
+
+    for key, value in data.iteritems():
+        if 'date' in key.lower():
+            # print 'found \'%s\' tag...'
+            dateTimeTags.append([key, value])
+            # print dateTimeTags
+
+    print 'All date and time related tags...'
+    for entry in dateTimeTags:
+        print '%s\t%s' % (entry[0], entry[1])
+
+
+    # find earliest dateTimeTag, that is most likely the date media was captured or created
+
+    earliestTag = ''
+    earliestDateTimeStamp = ''
+
+    for entry in dateTimeTags:
+        # default case
+        if len(earliestTag) < 1:
+            earliestTag = entry[0]
+            earliestDateTimeStamp = entry[1]
+
+        else:
+
+
+            dateTimeStamp = entry[1]
+            # remove timezone adjsutment if it exists
+            try:
+                dateTimeStamp.split('-')[0]
+            except:
+                pass
+
+            # break into components
+            mediaYear = dateTimeStamp.split(' ')[0].split(':')[0]
+            mediaMonth = dateTimeStamp.split(' ')[0].split(':')[1]
+            mediaDay = dateTimeStamp.split(' ')[0].split(':')[2]
+            mediaHour = edateTimeStamp.split(' ')[1].split(':')[0]
+            mediaMinute = dateTimeStamp.split(' ')[1].split(':')[1]
+            mediaSecond = dateTimeStamp.split(' ')[1].split(':')[2]
+
+            # if tag is later than existing earliest tag, skip
+            if True:
+                # check mediaYear
+
+                # check mediaMonth
+
+                # check mediaDay
+
+                # check mediaHour
+
+                # check mediaMinute
+
+                # check mediaSecond
+
+            # else, set as new earliest tag
+            else:
+                earliestTag = entry[0]
+                earliestDateTimeStamp = entry[1]
+
+
+
+def getCameraOperator(data, name):
+    print 'getting camera operator/photographer information...'
+
+
+
+
+
+
+
+
 
 #------------------------------------------------------------------------------
 #		main function
@@ -49,57 +144,49 @@ def main():
     parser = argparse.ArgumentParser(description='Read EXIF data ofa  given media file, update filename and sort into structured directory')
 
     # script uses the artist name to help createu uinique file names
-    parser.add_argument('-a', '--artistName', required=True, help='default artist name', metavar='ARTIST_NAME')
+    parser.add_argument('-a', '--artistName',
+                        required=True,
+                        help='default artist name',
+                        metavar='ARTIST_NAME')
 
     # passing a single file
-    parser.add_argument('-f', '--mediaFile', dest='mediaFile', required=False, help='pass a single file to process', metavar='MEDIA_FILE', type=lambda x: openMediaFile(parser, x))
+    parser.add_argument('-f', '--mediaFile', dest='mediaFile',
+                        required=False,
+                        help='pass a single file to process',
+                        metavar='MEDIA_FILE',
+                        type=lambda x: openMediaFile(parser, x))
 
     # passing a directory (with or without sub directories) of files
-    parser.add_argument('-d', '--mediaDirectory', dest='mediaDirectory', required=False, help='pass a directory of files to process, WARNING: RECURSIVE', metavar='MEDIA_DIRECTORY', type=lambda x: spacer())
+    parser.add_argument('-d', '--mediaDirectory', dest='mediaDirectory',
+                        required=False,
+                        help='pass a directory of files to process, WARNING: RECURSIVE',
+                        metavar='MEDIA_DIRECTORY', type=lambda x: spacer())
 
     args = vars(parser.parse_args())
 
     print 'filename: %s' % str(args['mediaFile']).split("'")[1]
     filename = str(args['mediaFile']).split("'")[1]
 
-    exifTagsDict = p.get_json(filename)
+    exifTagsDict = JSONToDict(p.get_json(filename))
 
     spacer()
-
-    print exifTagsDict
-
-    spacer()
-
     prettyPrintTags(exifTagsDict)
+    spacer()
+
+    getMediaDateTimeStamp(exifTagsDict)
 
     spacer()
 
-    prettyPrintTags2(exifTagsDict)
-
-    spacer()
 
 
-main()
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# fileName = '/Users/samgutentag/Downloads/IMG_1767.MOV'
-# data = p.get_json(fileName)
-# print (json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
-#
-#
-# print '\n'
-# print '#' + '-' * 79
-# print '\n'
-#
-# fileName = '/Users/samgutentag/Developer/python/mediaDelta/samplePhotos/phoneImage.JPG'
-# data = p.get_json(fileName)
-# print (json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
