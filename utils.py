f575cabdd5556f2fb4a81723359bf37ed33ed07f
Sam Gutentag <samgutentag@me.com>
2016-10-30 17:36:27 -0700

    adding empty utils.py supprt file

---
+++ utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/end python
+
+import argparse
+import os
+
+
+
+# this is pretty much a staging file, ill start moving stuff over here once
+# ive got it all working in a monolithic file
+
+
+# bad form... i know....
