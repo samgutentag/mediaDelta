63227f1508546fa94ed6fdc0cfbd3ce8698b5f31
Sam Gutentag <samgutentag@me.com>
2016-10-30 23:16:36 -0700

    string formatting for old and new file paths completed, next up cases to handle video files

--- pyExifToolTest.py
+++ pyExifToolTest.py
@@ -15,14 +15,13 @@ class cameraObject:
         self.make = make
         self.model = str(model).replace(' ','')
         self.serial = serial
-        self.software = str(software).split('(')[0]
+        self.software = str(software).split('(')[0].replace(' ', '').replace('.', '')
 
     def printInfo(self):
         print 'Camera Make:\t%s' % self.make
         print 'Camera Model:\t%s' % self.model
         print 'Serial Number:\t%s' % self.serial
         print 'Software:\t\t%s' % self.software
-
 
 class mediaDateTimeObject:
 
@@ -36,6 +35,12 @@ class mediaDateTimeObject:
         self.second = second
         self.millisecond = millisecond
 
+    def toString(self):
+        dateTimeString = '%s%s%s%s%s%s%s' % (self.year, self.month, self.day,
+                                            self.hour, self.minute, self.second,
+                                            self.millisecond)
+        return dateTimeString
+
     def printInfo(self):
         print 'tag:\t\t\t%s' % self.tag
         print 'year:\t\t\t%s' % self.year
@@ -45,9 +50,6 @@ class mediaDateTimeObject:
         print 'minute:\t\t\t%s' % self.minute
         print 'second:\t\t\t%s' % self.second
         print 'millisecond:\t%s' % self.millisecond
-
-
-
 
 #------------------------------------------------------------------------------
 #		functions
@@ -65,6 +67,215 @@ def openMediaFile(parser, arg):
         parser.error('The file %s does not exist' % arg)
     else:
         return open(arg, 'rb')
+
+def JSONToDict(data):
+    print '>>> creating exifDict from json data...'
+
+    if len(data) < 1:
+        print '>>> this file has no exif data'
+        return
+    else:
+        # dataDict = dict(sorted(data[0].iteritems()))
+        dataDict = dict(sorted(data[0].iteritems()))
+        print '>>> done!'
+        return dataDict
+
+# construct file path and name of 'correctly' named file
+def getFilePath(destinationDir, dateTimeStamp, cameraInfo, userName, extension):
+    fileName = ''
+    filePath = ''
+
+    # example : /DESTINATION_DIRECTORY/2016/10/31/
+    filePath = '%s/%s/%s/%s/' % (destinationDir, dateTimeStamp.year,
+                                                 dateTimeStamp.month,
+                                                 dateTimeStamp.day)
+
+    # camera model may not be known,
+    if cameraInfo.model == 'NONE':
+        # if it is not, try software,
+        if cameraInfo.software == 'NONE':
+            cameraName = ''
+        # if that is not known, leave blank
+        else:
+            cameraName = '_' + cameraInfo.software
+    else:
+        cameraName = '_' + cameraInfo.model
+
+    # example : 20161031120130000_username_cameramodel.extension
+    fileName = '%s_%s%s' % (dateTimeStamp.toString(), userName, cameraName)
+
+    extension = '.' + extension
+
+    return filePath + fileName + extension
+
+
+#------------------------------------------------------------------------------
+#		file relocation functions
+#------------------------------------------------------------------------------
+
+# gets contents of a directory
+def getDirectoryContents(dir):
+    print ">>> getting contents of directory '%s'" % dir
+
+    for item in dir:
+        # if item is a file
+        if os.path.isfile(item):
+            print '%s is a file!' % item
+
+        # if item is a directory
+        elif os.path.isdir(item):
+            print '%s is a directory!' % item
+            getDirectoryContents(item)
+
+        else:
+            print 'not sure what happened...'
+
+
+
+
+#------------------------------------------------------------------------------
+#		main exif tag formatting functions
+#------------------------------------------------------------------------------
+
+# compare two mediaDateTimeObjects and return earlier one
+def getEarlierDateTime(tagA, tagB):
+
+    # check components in order largest to smallest interval
+    # to see which came first
+    if int(tagA.year) < int(tagB.year):
+        return tagA
+    if int(tagA.month) < int(tagB.month):
+        return tagA
+    if int(tagA.day) < int(tagB.day):
+        return tagA
+    if int(tagA.hour) < int(tagB.hour):
+        return tagA
+    if int(tagA.minute) < int(tagB.minute):
+        return tagA
+    # this case is special because if tagB matches tagA, then stay with tagA
+    if int(tagA.second) <= int(tagB.second):
+        return tagA
+    else:
+        return tagB
+
+# gets earlist date time tag, excludes dates in photoshop or camera profiles
+# returns a list of (dateTimeTag, dateTimeValue)
+def getMediaDateTimeStamp(data):
+    print '>>> getting earliest media time stamp...'
+
+    # entryInfo = mediaDateTimeObject(tag, year, month, day, hour, minute, second, millisecond)
+    earlyDateTimeInfo = mediaDateTimeObject('NONE', '9999', '99', '99', '99', '99', '99', '999')
+
+    # collect all date related keys and values
+    dateTimeTags = []
+
+    # select dateTime key  value pairs as a subset
+    for key, value in data.iteritems():
+        if 'date' in key.lower():
+            # print 'found \'%s\' tag...'
+            if 'icc' not in key.lower():
+                dateTimeTags.append([key, value])
+
+    # go through dateTime tags to determine the earliest one
+    for entry in dateTimeTags:
+
+        # default values
+        entryInfo = mediaDateTimeObject('NONE', '9999', '99', '99', '99', '99', '99', '999')
+
+        # set entry tag
+        entryInfo.tag = str(entry[0])
+        dateTimeStamp = entry[1]
+
+        # remove timezone adjustment if it exists
+        try:
+            dateTimeStamp = dateTimeStamp.split('-')[0]
+        except:
+            pass
+
+        # try to split into dateStamp and timeStamp, get dateStamp
+        try:
+            dateStamp = dateTimeStamp.split(' ')[0]
+        except:
+            dateStamp = '9999:99:99'
+
+        # try to split into dateStamp and timeStamp, get timeStamp
+        try:
+            timeStamp = dateTimeStamp.split(' ')[1]
+
+            # sometimes time stamps have letters appened to the end of the second, lets remove those
+            while timeStamp[len(timeStamp)-1].isalpha():
+                timeStamp = timeStamp[:-1]
+        except:
+            timeStamp = '23:59:99'
+
+        # set values for each
+        entryInfo.year = dateStamp.split(':')[0]
+        entryInfo.month = dateStamp.split(':')[1]
+        entryInfo.day = dateStamp.split(':')[2]
+        entryInfo.hour = timeStamp.split(':')[0]
+        entryInfo.minute = timeStamp.split(':')[1]
+        entryInfo.second = timeStamp.split(':')[2]
+
+        # try splitting secondStamp into secondStamp and millisecondStamp
+        try:
+            entryInfo.millisecond = entryInfo.second.split('.')[1]
+            entryInfo.second = entryInfo.second.split('.')[0]
+
+            # some time stamps come through with an alpha character(s) suffix
+            # lets remove those if they exist
+            while entryInfo.millisecond[len(entryInfo.millisecond)-1].isalpha():
+                entryInfo.millisecond = entryInfo.millisecond[:-1]
+
+        except:
+            entryInfo.millisecond = '999'
+
+        # format millisecondStamp to be a fixed length
+        if len(entryInfo.millisecond) > 3:
+            entryInfo.millisecond = entryInfo.millisecond[:4]
+
+        while len(entryInfo.millisecond) < 3:
+            entryInfo.millisecond = '0' + entryInfo.millisecond
+
+        earlyDateTimeInfo = getEarlierDateTime(earlyDateTimeInfo, entryInfo)
+
+    print '>>> done!'
+    return earlyDateTimeInfo
+
+# gets camera and or software device information
+# returns a list of (cameraMake, cameraModel, serialNumber, softwareName)
+# defaults to 'NONE' if a piece of information can not be found
+def getCameraModel(data):
+    print '>>> getting camera info...'
+
+    # we want make, model, serial number, software
+    cameraMake = 'NONE'
+    cameraModel  = 'NONE'
+    serialNumber = 'NONE'
+    softwareName = 'NONE'
+
+    for key,value in data.iteritems():
+        # get serial number string i.e. '192029004068'
+        if 'exif:serialnumber' in key.lower():
+            serialNumber = str(data[key])
+        # get make string i.e. 'Canon'
+        if 'exif:make' in key.lower():
+            cameraMake = str(data[key])
+        # get model string i.e. 'Canon EOS 5D Mark III'
+        if 'exif:model' in key.lower():
+            cameraModel = str(data[key])
+        # get software string i.e. 'Adobe Photoshop Lightroom 6.3 (Macintosh)''
+        if 'exif:software' in key.lower():
+            softwareName = str(data[key])
+
+    # special case for iOS devices reporting software version
+    if cameraMake.lower() == 'apple':
+        softwareName = 'iOS %s' % softwareName
+
+    cameraInfo = cameraObject(cameraMake, cameraModel, serialNumber, softwareName)
+
+    print '>>> done!'
+
+    return cameraInfo
 
 # print tags
 def prettyPrintTags(dataDictionary):
@@ -101,274 +312,40 @@ def prettyPrintTags(dataDictionary):
             print entry
             counter += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     print '>>> done!'
 
-def JSONToDict(data):
-    print '>>> creating exifDict from json data...'
-
-    if len(data) < 1:
-        print '>>> this file has no exif data'
-        return
-    else:
-        # dataDict = dict(sorted(data[0].iteritems()))
-        dataDict = dict(sorted(data[0].iteritems()))
-        print '>>> done!'
-        return dataDict
-
-# gets contents of a directory
-def getDirectoryContents(dir):
-    print ">>> getting contents of directory '%s'" % dir
-
-    for item in dir:
-        # if item is a file
-        if os.path.isfile(item):
-            print '%s is a file!' % item
-
-        # if item is a directory
-        elif os.path.isdir(item):
-            print '%s is a directory!' % item
-            getDirectoryContents(item)
-
-        else:
-            print 'not sure what happened...'
-
-def getEarlierDateTime(tagA, tagB):
-
-    print 'TAG:\t\t\t%s\t%s' % (tagA.tag, tagB.tag)
-    print 'YEAR:\t\t\t%s\t%s' % (tagA.year, tagB.year)
-    print 'MONTH:\t\t\t%s\t\t%s' % (tagA.month, tagB.month)
-    print 'DAY:\t\t\t%s\t\t%s' % (tagA.day, tagB.day)
-    print 'HOUR:\t\t\t%s\t\t%s' % (tagA.hour, tagB.hour)
-    print 'MINUTE:\t\t\t%s\t\t%s' % (tagA.minute, tagB.minute)
-    print 'SECOND:\t\t\t%s\t\t%s' % (tagA.second, tagB.second)
-    print 'MILLISECOND:\t%s\t\t%s' % (tagA.millisecond, tagB.millisecond)
-
-    print '-' * 20
-
-
-
-    return tagB
-
-
-
-
-
-#------------------------------------------------------------------------------
-#		main exif tag formatting functions
-#------------------------------------------------------------------------------
-
-
-# gets camera and or software device information
-# returns a list of (cameraMake, cameraModel, serialNumber, softwareName)
-# defaults to 'NONE' if a piece of information can not be found
-def getCameraModel(data):
-    print '>>> getting camera info...'
-
-    # we want make, model, serial number, software
-    cameraMake = 'NONE'
-    cameraModel  = 'NONE'
-    serialNumber = 'NONE'
-    softwareName = 'NONE'
-
-    for key,value in data.iteritems():
-        # get serial number string i.e. '192029004068'
-        if 'exif:serialnumber' in key.lower():
-            serialNumber = str(data[key])
-        # get make string i.e. 'Canon'
-        if 'exif:make' in key.lower():
-            cameraMake = str(data[key])
-        # get model string i.e. 'Canon EOS 5D Mark III'
-        if 'exif:model' in key.lower():
-            cameraModel = str(data[key])
-        # get software string i.e. 'Adobe Photoshop Lightroom 6.3 (Macintosh)''
-        if 'exif:software' in key.lower():
-            softwareName = str(data[key])
-
-    # special case for iOS devices reporting software version
-    if cameraMake.lower() == 'apple':
-        softwareName = 'iOS %s' % softwareName
-
-    cameraInfo = cameraObject(cameraMake, cameraModel, serialNumber, softwareName)
-
-    # cameraInfo.printInfo()
-
-    print '>>> done!'
-
-    return cameraInfo
-
-# gets earlist date time tag, excludes dates in photoshop or camera profiles
-# returns a list of (dateTimeTag, dateTimeValue)
-def getMediaDateTimeStamp(data):
-    print '>>> getting earliest media time stamp...'
-
-    # # default date time information
-    tag = 'NONE'
-    year = '9999'
-    month = '99'
-    day = '99'
-    hour = '99'
-    minute = '99'
-    second = '99'
-    millisecond = '999'
-
-
-    entryInfo = mediaDateTimeObject(tag, year, month, day, hour, minute, second, millisecond)
-    earlyDateTimeInfo = mediaDateTimeObject(tag, year, month, day, hour, minute, second, millisecond)
-
-    # collect all date related keys and values
-    dateTimeTags = []
-
-    for key, value in data.iteritems():
-        if 'date' in key.lower():
-            # print 'found \'%s\' tag...'
-            if 'icc' not in key.lower():
-                dateTimeTags.append([key, value])
-
-    for entry in dateTimeTags:
-
-        entryInfo.tag = str(entry[0])
-        dateTimeStamp = entry[1]
-
-        # remove timezone adjustment if it exists
-        try:
-            dateTimeStamp = dateTimeStamp.split('-')[0]
-        except:
-            pass
-
-        # try to split into dateStamp and timeStamp, get dateStamp
-        try:
-            dateStamp = dateTimeStamp.split(' ')[0]
-        except:
-            dateStamp = '9999:99:99'
-
-        # try to split into dateStamp and timeStamp, get timeStamp
-        try:
-            timeStamp = dateTimeStamp.split(' ')[1]
-
-            # sometimes time stamps have letters appened to the end of the second, lets remove those
-            while timeStamp[len(timeStamp)-1].isalpha():
-                timeStamp = timeStamp[:-1]
-        except:
-            timeStamp = '23:59:99'
-
-        # update values for each
-        entryInfo.year = dateStamp.split(':')[0]
-        entryInfo.month = dateStamp.split(':')[1]
-        entryInfo.day = dateStamp.split(':')[2]
-        entryInfo.hour = timeStamp.split(':')[0]
-        entryInfo.minute = timeStamp.split(':')[1]
-        entryInfo.second = timeStamp.split(':')[2]
-
-        # try splitting secondStamp into secondStamp and millisecondStamp
-        try:
-            entryInfo.millisecond = entryInfo.second.split('.')[1]
-            entryInfo.second = entryInfo.second.split('.')[0]
-
-            # some time stamps come through with an alpha character(s) suffix
-            # lets remove those if they exist
-            while entryInfo.millisecond[len(entryInfo.millisecond)-1].isalpha():
-                entryInfo.millisecond = entryInfo.millisecond[:-1]
-
-        except:
-            entryInfo.millisecond = '999'
-
-        # format millisecondStamp to be a fixed length
-        if len(entryInfo.millisecond) > 3:
-            entryInfo.millisecond = entryInfo.millisecond[:4]
-
-        while len(entryInfo.millisecond) < 3:
-            entryInfo.millisecond = '0' + entryInfo.millisecond
-
-        # earlyDateTimeInfo = getEarlierDateTime(earlyDateTimeInfo, entryInfo)
-        early
-
-
-
-    print'\n\n'
-    print 'EARLIEST TAG IS: '
-    earlyDateTimeInfo.printInfo()
-
-
-
-    print '>>> done!!!'
-    # # return earlyDateTimeInfo
-    return True
-
-
-
-
-
-
-
-
-def getCameraOperator(data, name):
-    print '>>> getting camera operator/photographer information...'
-
-    print '>>> done!'
 
 #------------------------------------------------------------------------------
 #		file processing functions
 #------------------------------------------------------------------------------
-def processMediaFile(mediaFile):
-    fileName = str(mediaFile).split("'")[1]
-    print ">>> processing '%s'" % fileName
-    extension = str(fileName.split('.')[-1])
+def processMediaFile(mediaFile, userName):
+    originalFilePath = str(mediaFile).split("'")[1]
+    print ">>> processing '%s'" % originalFilePath
+    extension = str(originalFilePath.split('.')[-1])
 
     newFileName = ''
     newFilePath = ''
 
     # spacer()
-    exifTagsDict = JSONToDict(p.get_json(fileName))
+    exifTagsDict = JSONToDict(p.get_json(originalFilePath))
 
     spacer()
     dateTimeStamp = getMediaDateTimeStamp(exifTagsDict)
-    print dateTimeStamp
+    dateTimeStamp.printInfo()
 
     spacer()
     cameraInfo = getCameraModel(exifTagsDict)
+    cameraInfo.printInfo()
 
+    spacer()
+    prettyPrintTags(exifTagsDict)
 
-    # spacer()
-    # prettyPrintTags(exifTagsDict)
+    spacer()
+    destinationDir = '/DESTINATION_DIRECTORY'
+    correctedFilePath = getFilePath(destinationDir, dateTimeStamp, cameraInfo, userName, extension)
 
-    # all done!
-    # spacer()
-
-    year = dateTimeStamp[0]
-    month = dateTimeStamp[1]
-    day = dateTimeStamp[2]
-    hour = dateTimeStamp[3]
-    minute = dateTimeStamp[4]
-    second = dateTimeStamp[5]
-    millisecond = dateTimeStamp[6]
-
-    newFilePath = '/dest/%s/%s/%s/' % (year, month, day)
-    newFileName_Date = '%s%s%s%s%s%s%s' % (year, month, day, hour, minute, second, millisecond)
-    newFileName_User = 'samgutentag'
-
-
-    newFileName_Camera = '%s%s' % (cameraInfo.model, cameraInfo.serial)
-
-    newFileName = '%s_%s_%s' % (newFileName_Date, newFileName_User, newFileName_Camera)
-
-    destination = newFilePath + newFileName + '.' + extension
-
-    print destination
-
+    print originalFilePath
+    print correctedFilePath
 
 
     print '>>> done!'
@@ -380,13 +357,13 @@ def processMediaFile(mediaFile):
 #------------------------------------------------------------------------------
 
 def main():
-
     # setup parser
     parser = argparse.ArgumentParser(description='Read EXIF data ofa  given media file, update filename and sort into structured directory')
 
     # script uses the artist name to help createu uinique file names
-    parser.add_argument('-a', '--artistName',
+    parser.add_argument('-a', '--artistName', dest='artistName',
                         required=False,
+                        default=os.getlogin(),
                         help='default artist name',
                         metavar='ARTIST_NAME')
 
@@ -407,10 +384,10 @@ def main():
 
     print args
 
-    spacer()
+    # processMediaFile(args['mediaFile'], args['artistName'])
 
     try:
-        processMediaFile(args['mediaFile'])
+        processMediaFile(args['mediaFile'], args['artistName'])
     except:
         print '>>> no files to process... all done!'
 
